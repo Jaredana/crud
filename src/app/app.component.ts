6fbb2f6a74a509cbdeedae9734af7f9f9651014d
Jaredana <jaredana@pdx.edu>
2019-03-26 21:24:36 -0700

    added communication between angular and node api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RootService } from './root.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud';
+  constructor(private rootService : RootService) { }
+  ngOnInit() {
+    //testing get data
+    this.rootService.getAPIData().subscribe((response)=>{
+        console.log('response is ', response)
+    },(error) => {
+        console.log('error is ', error)
+    })
+    //testing post data
+    this.rootService.postAPIData().subscribe((response)=>{
+      console.log('response from post data is ', response);
+    },(error)=>{
+      console.log('error during post is ', error)
+    })
+
+  }
 }
---
+++ src/app/root.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class RootService {
+
+  constructor(private http: HttpClient) { }
+  getAPIData(){
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+  }
+  postAPIData(){
+    return this.http.post('/api/auth/register', {'name' : 'Code', 'email' : 'code@email.com', 'password' : 'testpassword'})
+  }
+}
