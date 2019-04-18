504ee66fce8561b34155c9b0125acb753be376fa
lib-techdesk <lib-techdesk@PSU.DS.PDX.EDU>
2019-04-18 15:25:58 -0700

    fixed deploy?

--- node/db.js
+++ node/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb+srv://plarpa:dexter48@cluster0-a6se3.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}, function(err, db) {
+mongoose.connect(MONGODB_URI='mongodb+srv://plarpa:dexter48@cluster0-a6se3.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}, function(err, db) {
     if (err) {
         console.log('Unable to connect to the server. Please start the server. Error:', err);
     } else {
