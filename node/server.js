f98e2835698efe2adafbb678060485295d590914
lib-techdesk <lib-techdesk@PSU.DS.PDX.EDU>
2019-04-17 23:19:39 -0700

    fixed deploy?

--- node/server.js
+++ node/server.js
@@ -6,8 +6,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Expose-Headers: X-Custom-header, token");
   next();
 });
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
