af652e3717a2c0ea5fa123e169d8a225813c2196
Jaredana <jaredana@pdx.edu>
2019-04-23 13:52:33 -0700

    fix deploy

--- node/app.js
+++ node/app.js
@@ -11,7 +11,7 @@ build a form in angular, and link it to register view, so users can be registere
 allow users to be viewed from angular page(pretty much just connect node and angular)
 */
 //need to serve index.html in dist to node serer, it is entrance to angular app
-var distDir = path.join('/app/crud' + '/dist/crud/');
+var distDir = path.join('/app' + '/dist/crud/');
 console.log('current directory: ' + __dirname)
 console.log('directory chose: ' + distDir);
 app.use(express.static(distDir));
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 
 app.get('*', (req,res) => {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(distDir + 'index.html');
 });
 //urls must have /api/ in front due to proxy config'd in angular app(to deal with CORS)
 
