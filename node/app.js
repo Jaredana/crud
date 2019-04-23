adf1da85b0dbe1af18a702a3e6bb16eac2aea443
lib-techdesk <lib-techdesk@PSU.DS.PDX.EDU>
2019-04-23 15:37:20 -0700

    deploy?

--- node/app.js
+++ node/app.js
@@ -5,15 +5,8 @@ var AuthController = require('./auth/authcontroller');
 var TicketController = require('./ticket/ticketcontroller');
 const path = require('path');
 var app = express();
-/* TODO:
-display a login page for root website directory '/'
-build a form in angular, and link it to register view, so users can be registered from angular site
-allow users to be viewed from angular page(pretty much just connect node and angular)
-*/
-//need to serve index.html in dist to node serer, it is entrance to angular app
+
 var distDir = path.join('/app' + '/dist/crud/');
-console.log('current directory: ' + __dirname)
-console.log('directory chose: ' + distDir);
 app.use(express.static(distDir));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,8 +16,8 @@ app.use(cookieParser())
 app.get('*', (req,res) => {
     res.sendFile(distDir + 'index.html');
 });
-//urls must have /api/ in front due to proxy config'd in angular app(to deal with CORS)
 
+//urls must have /api/ in front due to proxy config'd in angular app(to deal with CORS)
 
 app.use('/api/auth', AuthController);
 app.use('/api/ticket', TicketController);
